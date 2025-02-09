28ed550fe3e508eb55ef070bb58e845d9e7e092f
bibimeoww <papitchaya2404@gmail.com>
2025-02-10 00:32:30 +0700

    Required

--- player.h
+++ player.h
@@ -1,10 +1,43 @@
-#include <SFML/graphics.hpp>
+﻿#include <SFML/graphics.hpp>
 #include <random>
 #include <time.h>
+#ifndef PLAYER_H
+#define PLAYER_H
 
-Dir GetCorrection(Dir pdir, sf::Vertor2f ppos);
-void Correcing();
-void ResolveCollision();
-void PlayerMovement();
+#include "Pacman.h"
 
-#endif 
+namespace Player1 {
+	Dir GetCorrection(Dir pdir, sf::Vector2f ppos);
+	void Correcing();
+	void ResolveCollision();
+	void PlayerMovement();
+}
+// ฟังก์ชันสำหรับผู้เล่นคนที่1
+
+namespace Player2 {
+	Dir GetCorrection(Dir pdir, sf::Vertor2f ppos);
+	void Correcing();
+	void ResolveCollision();
+	void PlayerMovement();
+}
+// ฟังก์ชันสำหรับผู้เล่นคนที่2
+
+struct PlayerStats {};
+{
+	int score;
+	int lives;
+	bool isAlive;
+};
+// ฟังก์ชันสำหรับทั้งสองคน
+
+void IntializePlayers();
+void UpdatePlayerState();
+bool CheckPlayerCollision();
+void HandPLayerInteraction();
+
+
+#endif // !PLAYER_H
+
+
+
+ 
