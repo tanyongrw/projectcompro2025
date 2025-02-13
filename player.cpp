5e516bf93fb3ca7e59b98e4c29f86025685e4044
bibimeoww <papitchaya2404@gmail.com>
2025-02-13 23:33:08 +0700

    Required

--- player.cpp
+++ player.cpp
@@ -82,7 +82,7 @@ namespace Player1 {
 		if (!gState.player1->stopped) {
 			gState.player1->pos += dir_addition[gState.player->cur_dir] * player_speed;
 		}
-		if (gStateplayer1->cornering) {
+		if (gState.player1->cornering) {
 			Cornering();
 		}
 		if (PlayerTileCollision(gState.player1->cur_dir, gState.player1->pos)) {
@@ -97,6 +97,107 @@ namespace Player1 {
 		}
 		else if (gState.player1->pos.x >= 29) {
 			gState.player1->pos.x -= 29;
+		}
+	}
+}
+namespace Player2 {
+	Dir GetCorrection(Dir pdir, sf::Vector2f ppos)
+	{
+		switch (pdir)
+		{
+		case UP:
+		case DOWN:
+			if (ppos.x - (int)ppos.x >= 0.5)
+				return LEFT;
+			return RIGHT;
+		case LEFT:
+		case RIGHT:
+			if (ppos.y - (int)ppos.y >= 0.5)
+				return UO;
+			return DOWN;
+		}
+	}
+
+	void Corrnering()
+	{
+		gState.player2->pos += dir_addition[gState.player2->corrction] * player_speed;
+		bool done = false;
+		switch (gState.player2->correvtion)
+		{
+		case UP:
+			done = (gState.player2->pos.y - (int)gState.player2->pos.y <= 0.5);
+			break;
+		case DOWN:
+			done = (gState.player2->pos.y - (int)gState.player2->pos.y >= 0.5);
+			break;
+		case LEFT:
+			done = (gstate.player2->pos.x - (int)gstate.player2->pos.y <= 0.5);
+			break;
+		case RIGHT:
+			done = (gStateplayer2->pos.x - (int)gState.player2->pos.y >= 0.5);
+			break;
+		}
+		if (done) {
+			CenterObject(gState.player2->cur_dir, gState.player2->pos);
+			gState.player2->cornering = false;
+		}
+	}
+
+	void ResolveCollision()
+	{
+		switch (gState.player2->cur_dir) {
+		case UP:
+		case DOWN:
+			gState.player2->pos.y = (int)gState.player2->pos.y + 0.5;
+			break;
+		case LEFT:
+		case RIGHT:
+			gState.player2->pos.x = (int)gState.player2->pos.x + 0.5;
+			break;
+		}
+	}
+
+	void PlayerMovement()
+	{
+		Dir try_dir = NONE;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+			try_dir = UP;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			try_dir = DOWN;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			try_dir = RIGHT;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			try_dir = LEFT;
+
+		if (try_dir != NONE &&
+			!PlayerTileCollision(try_dir, gState.player2->pos) &&
+			!InTunnel(gState.player2->pos)) {
+			gState.player2->cur_dir = try_dir;
+			if (gState.player2->cur_dir != opposite_dir[gState.player2->cur_dir]) {
+				gState.player2->cornering = true;
+				gState.player2->correction = GetCorrection(try_dir, gState.player2->pos);
+			}
+		}
+
+		if (!gState.player2->stopprd) {
+			gState.player2->pos += dir_addition[gState.player2->cur_dir] * player_speed;
+		}
+
+		if (!gState.player2->stopped) {
+			gState.player2->pos += dir_addition[gState.player2->cur_dir] * player_speed;
+		}
+		if (PlayerTileCollision(gState.player2->cur_dir, gState.player2->pos)) {
+			ResolveCollision();
+			gState.player2->stopped = true;
+		}
+		else {
+			gState.player2->stopped = false;
+		}
+		if (gState.player2->pos.x < -1) {
+			gState.player->pos.x += 29;
+		}
+		else if (gState.player2->pos.x >= 29) {
+			gState.player2->pos.x -= 29;
 		}
 	}
 }
