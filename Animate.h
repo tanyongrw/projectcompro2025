27f1bc42d98f889cbf05b4c9afaa18452d709b26
tanyongrw <120167717+tanyongrw@users.noreply.github.com>
2025-02-22 22:14:26 +0700

    Update Animate.h

--- Animate.h
+++ Animate.h
@@ -1,1 +1,42 @@
 
+#ifndef ANIMATE_H
+#define ANIMATE_H
+
+#include "Pacman.h"
+#include <SFML/Graphics.hpp>
+
+class Animation {
+public:
+    Animation();
+    
+    void update(int ms_elapsed);
+    void startPacManDeath(int player);
+    void reset();
+    void setPacManMenuFrame();
+    
+    static sf::IntRect getGhostFrame(GhostType type, TargetState state, Dir dir);
+    static sf::IntRect getPacManFrame(Dir dir, int player);
+    
+    void pulseUpdate(int ms_elapsed);
+    bool isPulse() const;
+    void setPulseFrequency(int ms);
+
+private:
+    int pacman_frame;
+    int pacman_timer;
+    bool ascending;
+
+    bool ghost_frame_2;
+    int ghost_timer;
+
+    int fright_flash;
+    int energizer_timer;
+
+    bool death_animation;
+
+    int pulse;
+    int pulse_timer;
+    int pulse_limit;
+};
+
+#endif // ANIMATE_H
